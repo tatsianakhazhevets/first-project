72ba373c524040ab3b11879b826a73894cde1661
tatsiana <tatsiana.khazhevets@gmail.com>
2026-03-10 19:25:39 +0100

    Video Code Explanation

---
+++ src/main/java/no_more_manual_level_up_automation/practice_second/practice_codeExplanation/Main.java
@@ -0,0 +1,42 @@
+package no_more_manual_level_up_automation.practice_second.practice_codeExplanation;
+
+public class Main {    // объявили класс (модификатор доступа, ключевое слово class, имя класса)
+    // class declaration
+
+    public static void main(String[] args) { // сигнатура метода, объявление метода main
+        // method signature
+        // модификатор доступа
+        // модификатор static (main принадлежит классу, а не объекту)
+        // тип возвращаемых данных void
+        // имя метода main
+        // аргументы - тип массив строк, имя параметра
+        // тело метода, где реализуется задача
+
+        // объявление переменной - объявление типа и имени
+        // variable declaration
+        int count;
+        // инициализация - первое присваивания значения
+        // variable initialization
+        count = 0;
+        // объявляем переменную и инициализируем ее нулем
+        // we declared variable and initialized it with ziro value
+        int count2 = 0;
+        // присвоим новое значение переменной или обновим значение переменной
+        // we assign new value to count1 variable
+        count2 = 2;
+
+        //вызов метода по рассспечатыванию, в нем вызывается метод square
+        // вызов метода с контекстным значением параметра = аргумента
+        // here we call the method square with argument 4 and we printed out the result to te console with new line
+        System.out.println(square(4));
+    }
+// we defined a method signature square, which has public access modifier,
+// which has special modification static, which has return type primitive integer
+// and the name square. With parameter: type integet name x
+    // declared the body of the method
+    public static int square(int x) {  //сигнатура метода
+        // тело метода
+        return x * x;
+    }
+
+}
---
+++ src/main/java/no_more_manual_level_up_automation/practice_second/practice_codeExplanation/UserInfo.java
@@ -0,0 +1,40 @@
+package no_more_manual_level_up_automation.practice_second.practice_codeExplanation;
+
+public class UserInfo {
+    // class signature
+    // declare variable / final - not reassigned
+    // initialize the var DEFAULT_NAME with value unknown
+    private static final String DEFAULT_NAME = "unknown";
+    // after that we created special field name, which has ..
+    private String name; // объявление поля класса с типом данным String и именем name
+    private int age;
+
+    public void printInfo() {   // declared method with public access modifier .. withount any parameters
+        System.out.println("Имя " + name); // вызов распечатывания в консоль с новой строки с аргументом "Имя ", контентинированное с полем name
+        //method body, which contains printing in the console the name and afterword printing in the console the age
+        System.out.println("Возраст " + age);
+        // every line has to be separated as new line
+    }
+
+    // create two setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // create entry point main
+    public static void main(String[] args) {//определение метода
+        // create new variable with type UserInfo and name userInfo and initialazed it with
+        // new instance of UserInfo class, using special new operator
+        UserInfo userInfo = new UserInfo();
+        //call the method setName to set OLEG value as a name userInfo object
+        userInfo.setName("Олег");
+        // -\\-
+        userInfo.setAge(21);
+        // we call the method printInfo to check the result and to check we are gonna get the expected result
+        userInfo.printInfo();
+    }
+}
